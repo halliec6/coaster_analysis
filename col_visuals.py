39de5583b145aa969feb9ce4e31b639ae38def5f
halliec6 <halliec6@gmail.com>
2024-02-25 20:00:52 -0800

    correct diagrams

--- col_visuals.py
+++ col_visuals.py
@@ -6,20 +6,23 @@ from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 data = pd.read_csv("clean_total.csv")
 
 #cols we're analyzing
 dataframe = data[["Length (ft)", "Speed (mph)", "Height (ft)", "Inversions", "Drop (ft)"]]
-
 # box and whisker plots
 dataframe.plot(kind='box', subplots=True, layout=(3,2), sharex=False, sharey=False)
+plt.tight_layout()
 plt.savefig("clean_boxplots.png")
 
 # histograms
 dataframe.hist()
+plt.tight_layout()
 plt.savefig("clean_histograms.png")
 
 # scatter plot matrix
 scatter_matrix(dataframe)
+plt.tight_layout()
 plt.savefig("clean_scatterplots.png")
